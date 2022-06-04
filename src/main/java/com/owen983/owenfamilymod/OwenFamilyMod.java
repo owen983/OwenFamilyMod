bf4b3aa814a7516b5f57360735b28c9219810ee8
David Owen <d.owen983@gmail.com>
2022-06-03 21:30:37 -0400

    initial setup for beekeeper job block

--- src/main/java/com/owen983/owenfamilymod/OwenFamilyMod.java
+++ src/main/java/com/owen983/owenfamilymod/OwenFamilyMod.java
@@ -1,14 +1,16 @@
 package com.owen983.owenfamilymod;
 
+import com.owen983.owenfamilymod.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class OwenFamilyMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	public static final String MOD_ID = "owenfamilymod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -17,5 +19,6 @@ public class OwenFamilyMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/com/owen983/owenfamilymod/block/ModBlocks.java
@@ -0,0 +1,34 @@
+package com.owen983.owenfamilymod.block;
+
+import com.owen983.owenfamilymod.OwenFamilyMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModBlocks {
+
+    public static final Block OWEN_BEEKEEPER_BLOCK = registerBlock("owen_beekeeper_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.BEEHIVE).strength(5f).requiresTool()),
+            ItemGroup.MISC);
+
+    private static Block registerBlock(String name, Block block, ItemGroup itemGroup){
+        registerBlockItem(name, block, itemGroup);
+        return Registry.register(Registry.BLOCK, new Identifier(OwenFamilyMod.MOD_ID, name), block);
+    }
+    private static Item registerBlockItem(String name, Block block, ItemGroup group){
+        return Registry.register(Registry.ITEM, new Identifier(OwenFamilyMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(group)));
+    }
+    public static void registerModBlocks(){
+        OwenFamilyMod.LOGGER.info("registering ModBlocks for " + OwenFamilyMod.MOD_ID);
+    }
+
+
+}
